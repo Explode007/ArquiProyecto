eebf4f0154da543066c3220e0e5abdb5005acc21
gjsotero <gustavo.sotero@upr.edu>
2024-10-27 00:32:28 -0400

    Created CU MUX, Further Connections Established

--- PPU.v
+++ PPU.v
@@ -1,8 +1,6 @@
-//This is where all the wires are created and all components instantiated, essentially codifying the PPU
-
 `include "Adder.v"
 `include "ControlUnit.v"
-`include "mux.v"
+`include "cuMux.v"
 `include "PC.v"
 `include "rom.v"
 `include "pipeline_registers.v"
@@ -28,6 +26,37 @@ wire [31:0] Adder_Out;
 //ROM Wires
 wire [31:0] insMem_Out;
 
+//IF-ID Reg Wires
+reg [31:0] next_PC;
+wire [3:0] instruction_condition;
+wire [23:0] branch_offset;
+wire [31:0] next_pc_out;
+wire [3:0] ra;
+wire [3:0] rd;
+wire [3:0] rb;
+wire [11:0] imm_in;
+wire [31:0] cu_in;
+
+//Control Unit Wires
+wire rf_en;
+wire [3:0] alu_op;
+wire Load;
+wire branch_link;
+wire s_bit;
+wire rw;
+wire size;
+wire datamem_en;
+
+//Control Unit Mux Wires
+wire rf_en_out;
+wire [3:0] alu_op_out;
+wire Load_out;
+wire branch_link_out;
+wire s_bit_out;
+wire rw_out;
+wire size_out;
+wire datamem_en_out;
+
 //Instantiations here!
 
 PC PCReg(
@@ -49,11 +78,120 @@ rom insMem(
 );
 
 ControlUnit CU(
-
+    .instruction(cu_in),
+    .rf_en(rf_en),
+    .alu_op(alu_op),
+    .Load(Load),
+    .branch_link(branch_link),
+    .s_bit(s_bit),
+    .rw(rw),
+    .size(size),
+    .datamem_en(datamem_en)
 );
 
-pipeline_registers PR(
+cuMux Mux(
+    .s(s),
+    .rf_en_in(rf_en),
+    .alu_op_in(alu_op),
+    .Load_in(Load),
+    .branch_link_in(branch_link),
+    .s_bit_in(s_bit),
+    .rw_in(rw),
+    .size_in(size),
+    .datamem_en_in(datamem_en),
+    .rf_en_out(rf_en_out),
+    .alu_op_out(alu_op_out),
+    .Load_out(Load_out),
+    .branch_link_out(branch_link_out),
+    .s_bit_out(s_bit_out),
+    .rw_out(rw_out),
+    .size_out(size_out),
+    .datamem_en_out(datamem_en_out)
+);
 
+if_id_reg IF_ID(
+    .clk(clk),
+    .load_enable(LE),
+    .reset(rst),
+    .instruction(PC_Out),
+    .next_pc(next_PC),
+    .instruction_condition(instruction_condition),
+    .branch_offset(branch_offset),
+    .next_pc_out(next_pc_out),
+    .ra(ra),
+    .rd(rd),
+    .rb(rb),
+    .imm_in(imm_in),
+    .cu_in(cu_in)
+);
+
+id_exe_reg ID_EXE(
+    .clk(clk), 
+    .reset(rst),
+    .mux_control,
+    .instruction_condition,
+    .next_pc,
+    .operand_a,
+    .operand_b,
+    .operand_d,
+    .rd,
+    .imm_in,
+    .am,
+    .alu_op,
+    .rf_en,
+    .s,
+    .datamem_en,
+    .readwrite,
+    .size,
+    .load_instruction,
+    .mux_control_out,
+    .instruction_condition_out,
+    .next_pc_out,
+    .operand_a_out,
+    .operand_b_out,
+    .operand_d_out,
+    .rd_out,
+    .imm_in_out,
+    .am_out,
+    .alu_op_out,
+    .rf_en_out,
+    .s_out,
+    .datamem_en_out,
+    .readwrite_out,
+    .size_out,
+    .load_instruction_out
+);
+
+exe_mem_reg EXE_MEM(
+    .clk(clk),
+    .reset(rst),
+    .alu_or_nextpc,
+    .data_in,
+    .rd,
+    .rf_en,
+    .datamem_en,
+    .readwrite,
+    .size,
+    .load_instruction,
+    .alu_or_nextpc_out,
+    .data_in_out,
+    .rd_out,
+    .rf_en_out,
+    .datamem_en_out,
+    .readwrite_out,
+    .size_out,
+    .load_instruction_out
+);
+
+mem_wb_reg MEM_WB(
+    .clk(clk),
+    .reset(rst),
+    .rd,
+    .data,
+    .rf_en,
+    .rd_out,
+    .data_out,
+    .rf_en_out
 );
 
 //Simulation stuff here!
@@ -67,6 +205,10 @@ initial begin
         Address = Address + 1;
     end
     $fclose(fi);
+end
+
+initial begin
+    LE = 1'b1;
 end
 
 initial begin
---
+++ cuMux.v
@@ -0,0 +1,43 @@
+module cuMux (
+    input s;
+    input rf_en_in,          
+    input [3:0] alu_op_in,   
+    input Load_in,            
+    input branch_link_in,    
+    input s_bit_in,          
+    input rw_in,             
+    input size_in,           
+    input datamem_en_in,
+    output reg rf_en_out,          
+    output reg [3:0] alu_op_out,   
+    output reg Load_out,            
+    output reg branch_link_out,   
+    output reg s_bit_out,          
+    output reg rw_out,             
+    output reg size_out,           
+    output reg datamem_en_out
+);
+
+    always @* begin
+        if(s == 1'b0) begin // Pass Control Unit values when selector is 0
+            rf_en_out <= rf_en_in;
+            alu_op_out <= alu_op_in;
+            Load_out <= Load_in;
+            branch_link_out <= branch_link_in;
+            s_bit_out <= s_bit_in;
+            rw_out <= rw_in;
+            size_out <= size_in;
+            datamem_en_out <= datamem_en_in;
+        end 
+        else begin
+            rf_en_out <= 1'b0;
+            alu_op_out <= 1'b0;
+            Load_out <= 1'b0;
+            branch_link_out <= 1'b0;
+            s_bit_out <= 1'b0;
+            rw_out <= 1'b0;
+            size_out <= 1'b0;
+            datamem_en_out <= 1'b0;
+        end
+    end
+endmodule
