2763e1ce2b0710f5cb65765a93df981287f639a7
gjsotero <gustavo.sotero@upr.edu>
2024-11-16 18:44:51 -0400

    Added RAM to PPU and the file precharge.txt

--- Fase 4/PF4_SAMUEL_ANTHONY_GUSTAVO_PPU.v
+++ Fase 4/PF4_SAMUEL_ANTHONY_GUSTAVO_PPU.v
@@ -1038,6 +1038,43 @@ endmodule
         end
     endmodule
 
+    module ram (
+    output reg [31:0] DataOut,  
+    input E,               
+    input RW,                   
+    input [8:0] A,              
+    input [31:0] DataIn,        
+    input Size           
+);
+
+    reg [7:0] Mem [0:255];  
+
+  always @(E) begin
+        if (RW == 0) begin  
+                case (Size)
+                    0: DataOut = {24'b0, Mem[A]};  //Read Byte
+
+                    1: DataOut = {Mem[A], Mem[A+1], Mem[A+2], Mem[A+3]};   //Read Word
+                endcase
+            end
+
+        if (RW == 1) begin  
+            case (Size)
+                0: Mem[A] = DataIn[7:0]; //Write Byte
+                
+                1: begin                 //Write Word
+                    Mem[A] = DataIn[7:0];   
+                    Mem[A+1] = DataIn[15:8];
+                    Mem[A+2] = DataIn[23:16];
+                    Mem[A+3] = DataIn[31:24]; 
+                end
+            endcase
+        end
+    end
+
+    endmodule
+
+
     module if_id_reg(
         input clk, load_enable,reset,
         input [31:0] instruction, 
