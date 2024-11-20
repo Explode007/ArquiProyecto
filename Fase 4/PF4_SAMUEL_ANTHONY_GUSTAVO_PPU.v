be5729dfd6f2a19d48774f3536c93f952a41955d
gjsotero <gustavo.sotero@upr.edu>
2024-11-19 20:33:23 -0400

    Changed RAM's A input to 8 bits

--- Fase 4/PF4_SAMUEL_ANTHONY_GUSTAVO_PPU.v
+++ Fase 4/PF4_SAMUEL_ANTHONY_GUSTAVO_PPU.v
@@ -513,7 +513,7 @@ module PPU();
                 .DataOut(dataMem_Out),
                 .E(datamem_en_out_exemem),
                 .RW(rw_out_exemem),
-                .A(AluORNextPC_out_exemem[8:0]),
+                .A(AluORNextPC_out_exemem[7:0]),
                 .DataIn(OperandD_out_exemem),
                 .Size(size_out_exemem)
             );
@@ -1067,7 +1067,7 @@ endmodule
         output reg [31:0] DataOut,  
         input E,               
         input RW,                   
-        input [8:0] A,              
+        input [7:0] A,              
         input [31:0] DataIn,        
         input Size           
         );
